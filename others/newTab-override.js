bbbf90a665fcc73d652ffed5d96a3489f9d2738d
HibouDev <ahmed.rarhibou@outlook.com>
2025-09-18 10:43:30 -0400

    Update newTab-override.js

--- others/newTab-override.js
+++ others/newTab-override.js
@@ -1,5 +1,22 @@
-chrome.tabs.onCreated.addListener(function(tab) {
-  if (tab.url === "chrome://newtab/") {
-    chrome.tabs.update(tab.id, { url: "your-custom-page.html" });
-  }
+//=+=+=+=+=+=+=+=+=+=+=
+//      Config
+//=+=+=+=+=+=+=+=+=+=+=
+let tabURL = ""; // Enter the URL of the new "New Tab" page(you can use a extention url)
+
+// Override with custom page
+var TabOverride = function (id, url) {
+    if (typeof url == "undefined") return;
+    if (url.search(/(chrome|opera|vivaldi):\/.*\/startpage/ig) == -1) return;
+    if (url.search(/(download|history|bookmark|extension|news)/ig) != -1) return;
+
+    var parser = new URL(tabURL);
+
+    chrome.tabs.create({ url: parser.href, active: true });
+    chrome.tabs.remove(id);
+    console.log("[📦] Created new tab on" + url);
+};
+
+// Event listener for tab creation
+chrome.tabs.onCreated.addListener(function (tab) {
+    TabOverride(tab.id, tab.url || tab.pendingUrl);
 });
